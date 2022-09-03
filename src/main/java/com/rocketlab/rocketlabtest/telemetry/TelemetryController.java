6be059eb71e986b1aabb82272ee96fb73d0bdf1b
Cory Reed <pyrotrojan@gmail.com>
2022-09-03 17:24:34 -0400

    More restful changes

--- src/main/java/com/rocketlab/rocketlabtest/telemetry/TelemetryController.java
+++ src/main/java/com/rocketlab/rocketlabtest/telemetry/TelemetryController.java
@@ -2,10 +2,11 @@ package com.rocketlab.rocketlabtest.telemetry;
 
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.IanaLinkRelations;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
@@ -14,28 +15,32 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 @RequestMapping("/telemetry")
 public class TelemetryController {
 
-    private final TelemetryRepository telemetryRepository;
+    private final TelemetryRepository repository;
 
-    public TelemetryController(TelemetryRepository telemetryRepository) {
-        this.telemetryRepository = telemetryRepository;
+    private final TelemetryModelAssembler assembler;
+
+    public TelemetryController(TelemetryRepository repository, TelemetryModelAssembler assembler) {
+        this.repository = repository;
+        this.assembler = assembler;
     }
 
     @PostMapping
-    public Telemetry newTelemetry(@RequestBody Telemetry telemetry) {
-        return telemetryRepository.save(telemetry);
+    public ResponseEntity<EntityModel<Telemetry>> newTelemetry(@RequestBody Telemetry telemetry) {
+        EntityModel<Telemetry> entityModel = assembler.toModel(repository.save(telemetry));
+        return ResponseEntity
+                .created(entityModel.getRequiredLink(IanaLinkRelations.SELF).toUri())
+                .body(entityModel);
     }
 
     @PostMapping("/bulk")
     public List<Telemetry> newBulkTelemetry(@RequestBody List<Telemetry> telemetries) {
-        return telemetryRepository.saveAll(telemetries);
+        return repository.saveAll(telemetries);
     }
 
     @GetMapping
     public CollectionModel<EntityModel<Telemetry>> all() {
-        List<EntityModel<Telemetry>> telemetries = telemetryRepository.findAll().stream()
-                .map(telemetry -> EntityModel.of(telemetry,
-                        linkTo(methodOn(TelemetryController.class).byId(telemetry.getId())).withSelfRel(),
-                        linkTo(methodOn(TelemetryController.class).all()).withRel("telemetry")))
+        List<EntityModel<Telemetry>> telemetries = repository.findAll().stream()
+                .map(assembler::toModel)
                 .toList();
         return CollectionModel.of(telemetries,
                 linkTo(methodOn(TelemetryController.class).all()).withSelfRel());
@@ -43,31 +48,30 @@ public class TelemetryController {
 
     @GetMapping("/{id}")
     public EntityModel<Telemetry> byId(@PathVariable Long id) {
-        Telemetry telemetry = telemetryRepository.findById(id)
+        Telemetry telemetry = repository.findById(id)
                 .orElseThrow(() -> new TelemetryNotFoundException(id));
-        return EntityModel.of(telemetry,
-                linkTo(methodOn(TelemetryController.class).byId(id)).withSelfRel(),
-                linkTo(methodOn(TelemetryController.class).all()).withRel("telemetry"));
+        return assembler.toModel(telemetry);
     }
 
     @PutMapping("/{id}")
     public Telemetry replaceTelemetry(@RequestBody Telemetry telemetry, @PathVariable Long id) {
-        return telemetryRepository.findById(id)
+        return repository.findById(id)
                 .map(tel -> {
                     tel.setMeasurement(telemetry.getMeasurement());
                     tel.setCraftId(telemetry.getCraftId());
                     tel.setPosition(telemetry.getPosition());
                     tel.setTime(telemetry.getTime());
-                    return telemetryRepository.save(tel);
+                    return repository.save(tel);
                 })
                 .orElseGet(() -> {
                     telemetry.setId(id);
-                    return telemetryRepository.save(telemetry);
+                    return repository.save(telemetry);
                 });
     }
 
     @DeleteMapping("/{id}")
-    public void deleteTelemetry(@PathVariable Long id) {
-        telemetryRepository.deleteById(id);
+    public ResponseEntity<?> deleteTelemetry(@PathVariable Long id) {
+        repository.deleteById(id);
+        return ResponseEntity.noContent().build();
     }
 }
---
+++ src/main/java/com/rocketlab/rocketlabtest/telemetry/TelemetryModelAssembler.java
@@ -0,0 +1,24 @@
+package com.rocketlab.rocketlabtest.telemetry;
+
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+@Component
+public class TelemetryModelAssembler implements RepresentationModelAssembler<Telemetry, EntityModel<Telemetry>> {
+    @Override
+    public EntityModel<Telemetry> toModel(Telemetry telemetry) {
+        return EntityModel.of(telemetry, //
+                linkTo(methodOn(TelemetryController.class).byId(telemetry.getId())).withSelfRel(),
+                linkTo(methodOn(TelemetryController.class).all()).withRel("employees"));
+    }
+
+    @Override
+    public CollectionModel<EntityModel<Telemetry>> toCollectionModel(Iterable<? extends Telemetry> entities) {
+        return RepresentationModelAssembler.super.toCollectionModel(entities);
+    }
+}
